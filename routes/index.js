83c1ffebc92c8ad13721706c09642d71eb943021
George Bockari <bitwhys@gmail.com>
2022-12-30 02:00:14 -0500

    dev: hmr (sorta) is working correctly

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Learn Authentication' })
+  res.render('index', { heading: 'Finish what you started today before you forget to do it.' })
 })
 
 module.exports = router
